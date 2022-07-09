8def4f2b32a173315a119e3517523f15db49e4ef
md-omarfaruk <87226097+md-omarfaruk@users.noreply.github.com>
2022-07-09 10:38:41 +0600

    celcius to fahrenheit

---
+++ celciusToFahrenheit/celciusToFahrenheit.js
@@ -0,0 +1,13 @@
+// steps
+// *Accept a number from user(Temperature in Celcius)
+// *Convert it to Fahrenheit(Formula: F = 9/5 * C + 32)
+// *Print the result as alert! (example: "Fahrenheit: 94 Degree")
+// *Print "Task Complete" in console.
+
+let celcius = prompt("Please enter the Celcius number");
+
+const fahrenheit = 9/5 * celcius + 32;
+
+alert("Fahrenheit: " + fahrenheit + " Degree");
+
+console.log("Task Complete");
